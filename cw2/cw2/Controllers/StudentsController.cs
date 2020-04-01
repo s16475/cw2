1aeceae8f19affe0198156c49f33283ecadeda7f
grzesiek <s16475@pjwstk.edu.pl>
2020-04-01 14:37:10 +0200

    zad 4.5 (3.5) - poprawka do sql injection

--- cw2/cw2/Controllers/StudentsController.cs
+++ cw2/cw2/Controllers/StudentsController.cs
@@ -53,8 +53,9 @@ namespace cw2.Controllers
         }
 
 
-        // Zad. 4.3
 
+        // Zad. 4.3
+        /*
         [HttpGet("{id}")]
         public IActionResult GetStudent(string id)
         {
@@ -81,10 +82,42 @@ namespace cw2.Controllers
 
             return Ok(student);
         }
+        */
 
         // Zad. 4.4
         // zapytanie sql injection
         // https://localhost:5001/api/students/' ;DROP TABLE Student;--
+
+        // Zad 4.5
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(string id)
+        {
+            var student = new StudentInfoDTO();
+            using (SqlConnection connection = new SqlConnection(ConString))
+            using (SqlCommand command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = "select s.FirstName, s.LastName, s.BirthDate, s.IndexNumber, st.Name, e.Semester from Student s " +
+                    "join Enrollment e on e.IdEnrollment = s.IdEnrollment join Studies st on st.IdStudy = e.IdStudy " +
+                    $"where s.IndexNumber = @index";
+
+                command.Parameters.AddWithValue("index", id);
+
+                connection.Open();
+
+                SqlDataReader dataReader = command.ExecuteReader();
+                dataReader.Read();
+
+                student.FirstName = dataReader["FirstName"].ToString();
+                student.LastName = dataReader["LastName"].ToString();
+                student.Name = dataReader["Name"].ToString();
+                student.BirthData = dataReader["BirthDate"].ToString();
+                student.Semester = dataReader["Semester"].ToString();
+
+            }
+
+            return Ok(student);
+        }
 
         // Zad 4.1
         /*
