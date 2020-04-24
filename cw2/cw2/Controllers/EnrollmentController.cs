4792ddc0ef2d2f257338886689da98bb9aeddfcd
grzesiek <s16475@pjwstk.edu.pl>
2020-04-24 21:24:56 +0200

    ukonczono zad. 5.3 (4.3)

--- cw2/cw2/Controllers/EnrollmentController.cs
+++ cw2/cw2/Controllers/EnrollmentController.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Globalization;
 using System.Net;
 using cw2.DTOs.Requests;
+using cw2.Services;
 using cw2.DTOs.Responses;
 using cw2.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -15,156 +16,29 @@ namespace cw2.Controllers
     [ApiController]
     public class EnrollmentController : ControllerBase
     {
-        private const string ConString = "Server=localhost;Database=master;User Id = sa; Password=B2@X7A89;";
+        private readonly IStudentsDbService _service;
+
+        public EnrollmentController(IStudentsDbService _service)
+        {
+            this._service = _service;
+        }
 
         //zadanie 4.1 (5.1) - dodajemy nowych studentow
         [HttpPost]
         public IActionResult EnrollStudent(EnrollStudentRequest request)
         {
-            EnrollStudentResponse esr = new EnrollStudentResponse() { };
-
-            using (var con = new SqlConnection(ConString))
-            using (var com = new SqlCommand())
-            {
-                con.Open();
-                var tran = con.BeginTransaction();
-                com.Connection = con;
-                com.Transaction = tran;
-                try
-                {
-
-                    //1. Czy studia istnieją?
-
-                    com.CommandText = "SELECT IdStudy AS idStudies FROM Studies WHERE Name=@name";
-                    com.Parameters.AddWithValue("name", request.Studies);
-                    var dr = com.ExecuteReader();
-                    if (!dr.Read())
-                    {
-                        dr.Close();
-                        tran.Rollback();
-                        return NotFound("Nie ma takich studiów");
-
-                    }
-
-                    int idStudies = (int)dr["idStudies"];
-                    dr.Close();
-
-                    //2. Sprawdzenie czy nie występuje konflikt indeksów  
-
-                    com.CommandText = "SELECT IndexNumber FROM Student WHERE IndexNumber = '" + request.IndexNumber + "'";
-                    dr = com.ExecuteReader();
-                    if (dr.Read())
-                    {
-                        dr.Close();
-                        tran.Rollback();
-                        return BadRequest("Student z takim indeksem już istnieje");
-                    }
-                    dr.Close();
-
-                    // 3.Nadanie i wstawienie IdEnrollment
-
-                    int idEnrollment;
-                    com.CommandText = "SELECT IdEnrollment FROM Enrollment WHERE IdEnrollment = " +
-                                            "(Select max(IdEnrollment) from Enrollment)";
-                    dr = com.ExecuteReader();
-                    if (!dr.Read())
-                    {
-                        idEnrollment = 1;
-                        dr.Close();
-                        com.CommandText = "INSERT INTO Enrollment(IdEnrollment,Semester,IdStudy,StartDate)" +
-                        "  VALUES(" + idEnrollment + ", 1, " + idStudies + ",GetDate())";
-                        com.ExecuteNonQuery();
-                    }
-                    idEnrollment = (int)dr["idEnrollment"];
-                    dr.Close();
-
-                    //4. Wstawienie studenta
-
-                    string strDateFormat = "dd.MM.yyyy";
-                    DateTime BirthDate = DateTime.ParseExact(request.BirthDate.ToString(), strDateFormat, CultureInfo.InvariantCulture);
-
-                    com.CommandText = $"INSERT INTO Student VALUES (@IndexNumber, @FirstName, @LastName, @BirthDate, @IdEnrollment)";
-                    com.Parameters.AddWithValue("IndexNumber", request.IndexNumber);
-                    com.Parameters.AddWithValue("FirstName", request.FirstName);
-                    com.Parameters.AddWithValue("LastName", request.LastName);
-                    com.Parameters.AddWithValue("BirthDate", BirthDate);
-                    com.Parameters.AddWithValue("IdEnrollment", idEnrollment);
-                    com.ExecuteNonQuery();
-
-                    esr.IdEnrollment = idEnrollment;
-                    esr.IdStudy = idStudies;
-                    esr.Semester = 1;
-                    esr.StartDate = DateTime.Now;
-                    tran.Commit();
-                    tran.Dispose();
-                    return StatusCode((int)HttpStatusCode.Created, esr);
-
-                }
-                catch (SqlException exc)
-                {
-                    tran.Rollback();
-                    return BadRequest(exc.Message);
-                }
-            }
+            var result = _service.EnrollStudent(request);
+            if (result != null) return Ok(result);
+            return NotFound();
         }
 
         //zadanie 4.2 (5.2) - promocja na nowy semestr
         [HttpPost("promotions")]
         public IActionResult PromoteStudent(PromoteStudentRequest promote)
         {
-            Enrollment enrollment = new Enrollment();
-            using (SqlConnection con = new SqlConnection(ConString))
-            using (SqlCommand com = new SqlCommand())
-            {
-                com.Connection = con;
-                con.Open();
-
-
-                com.Parameters.AddWithValue("Studies", promote.Studies);
-                com.Parameters.AddWithValue("Semester", promote.Semester);
-                var wynik = UseProcedure("PromoteStudents", com);
-
-                if (wynik[0][0].Equals("404"))
-                {
-                    return new NotFoundResult();
-                }
-                enrollment.IdEnrollment = wynik[0][0];
-                enrollment.IdStudy = wynik[0][2];
-                enrollment.Semester = wynik[0][1];
-                enrollment.StartDate = wynik[0][3];
-
-            }
-
-            ObjectResult objectResult = new ObjectResult(enrollment);
-            objectResult.StatusCode = 201;
-            return objectResult;
-
-        }
-
-        public List<string[]> UseProcedure(string name, SqlCommand com)
-        {
-            List<string[]> wynik = new List<string[]>();
-
-            com.CommandType = CommandType.StoredProcedure;
-            com.CommandText = name;
-            if (com.Connection.State != ConnectionState.Open) com.Connection.Open();
-
-            var dr = com.ExecuteReader();
-
-            while (dr.Read())
-            {
-                string[] tmp = new string[dr.FieldCount];
-                for (int i = 0; i < dr.FieldCount; i++)
-                {
-                    tmp[i] = dr.GetValue(i).ToString();
-                }
-                wynik.Add(tmp);
-            }
-            dr.Close();
-            com.CommandType = CommandType.Text;
-            com.Parameters.Clear();
-
-            return wynik;
+            var result = _service.PromoteStudent(promote);
+            if (result != null) return Ok(result);
+            return NotFound(); 
         }
     }
 }
--- cw2/cw2/Startup.cs
+++ cw2/cw2/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using cw2.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,7 @@ namespace cw2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IStudentsDbService, SqlServerDbService>();
             services.AddControllers();
         }
 
